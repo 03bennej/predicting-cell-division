a4e802128f3f16737dbb67329e067992537229ef
03bennej <jjrbenn@gmail.com>
2023-12-16 07:17:48 +0000

    files for venv

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name='src', version='0.0', packages=find_packages())
+setup(name='src',       # name of package
+      version='0.0',    # version of package
+      packages=find_packages())     # looks for all __init__.py files in directory
